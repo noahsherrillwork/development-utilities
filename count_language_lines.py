f11d8760829e9a0ef3a235e5d4c6b090b42127be
Noah Sherrill <noah.sherrill@liferay.com>
2017-11-17 12:21:34 -0800

    Added rough counts for words and unique words

--- count_language_lines.py
+++ count_language_lines.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright (c) 2000-present Liferay, Inc. All rights reserved.
 #
 # This library is free software; you can redistribute it and/or modify it under
@@ -12,17 +14,39 @@
 
 import os
 
+def get_words(line):
+	line_parts = line.split('=')
+	words = line_parts[1].split(' ')
+	return words
+
+
+def get_unique_words(words):
+	unique_words = set()
+	for word in words:
+		unique_words.add(word.lower())
+
+	return unique_words
+
+
 total_lines = 0
+words = []
 
-for (root, dirs, files) in os.walk('liferay-portal'):
-    #print(root)
-    for file_name in files:
-        if file_name == 'Language.properties' and '\\src\\' in root:
-            language_file = open(os.path.join(root, file_name))
-            language_lines = language_file.readlines()
-            for line in language_lines:
-                if '=' in line:
-                    total_lines += 1
-            language_file.close()
+for (root, dirs, files) in os.walk('.'):
+	print(root)
+	for file_name in files:
+		src_path_component = os.sep + 'src' + os.sep
+		if file_name == 'Language.properties' and src_path_component in root:
+			language_file = open(os.path.join(root, file_name))
+			language_lines = language_file.readlines()
+			for line in language_lines:
+				if '=' in line:
+					total_lines += 1
+					words.extend(get_words(line))
+			language_file.close()
 
-print(total_lines)
+print('Total lines = %d' % total_lines)
+print('Total words = %d' % len(words))
+
+unique_words = get_unique_words(words)
+
+print('Total unique words = %d' % len(unique_words))
